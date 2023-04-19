5f7846b92919c496d980a342a71f5df6f79d1b8a
hironor1 <halell2021b@gmail.com>
2023-04-19 20:03:28 +0900

    add knock01-09

---
+++ hironori/chapter01/knock01.py
@@ -0,0 +1,5 @@
+s = "パタトクカシーー"
+t = ""
+for i in range(0, len(s), 2):
+    t += s[i]
+print(t)
---
+++ hironori/chapter01/knock02.py
@@ -0,0 +1,5 @@
+s, t = "パトカー", "タクシー"
+u = ""
+for i in range(len(s)):
+    u += s[i] + t[i]
+print(u)
