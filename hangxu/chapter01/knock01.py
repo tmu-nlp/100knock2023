56a73d61738d2d16b6b3be154989a25fbc99a6b8
[hangxu] <[zhouhx0430@163.com]>
2023-04-21 14:27:29 +0900

    .

---
+++ hangxu/chapter01/knock01.py
@@ -0,0 +1,5 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+a='パタトクカシーー'
+print(a[0],a[2],a[4],a[6])
+パ ト カ ー
