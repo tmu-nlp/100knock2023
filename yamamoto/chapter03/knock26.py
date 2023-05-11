584252d9758b1eb7aa7fc6fee8608dc46352ee2b
Yuto-Yamamoto01 <yy.edu1619@gmail.com>
2023-05-11 22:45:41 +0900

    chapter03 25~29

--- yamamoto/chapter03/knock26.py
+++ yamamoto/chapter03/knock26.py
@@ -5,6 +5,8 @@ with open('jawiki-country.json', 'r') as f:
     for line in f:
         country = json.loads(line)
         if country['title'] == 'イギリス':
-            info = re.search(r'\{\{基礎情報.*\n\}\}', country['text'], re.DOTALL+re.MULTILINE).group()
+            info = re.search(r'\{\{基礎情報.*\n\}\}', country['text'], re.DOTALL).group()
             dictionary = dict(re.findall(r'\|(.*?)=(.*?)\n', info, re.DOTALL))
+            for k, v in dictionary.items():
+                dictionary[k] = re.sub(r'\'{2,5}', '', v)
             print(dictionary)
