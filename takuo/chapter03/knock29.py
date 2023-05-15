4a49a63fd441b9a7c942a289f8c961d257c49c14
tenk-laptop <takuo6000@gmail.com>
2023-05-15 16:05:10 +0900

    fix repo

--- takuo/chapter03/knock29.py
+++ takuo/chapter03/knock29.py
@@ -1,4 +1,4 @@
-
+import requests
 import re
 
 
@@ -41,4 +41,8 @@ with open("chapter03/uk_text.txt", 'r') as uktxt:
     for key in baseinfo.keys():
         baseinfo[key] = getEssence(baseinfo[key])
     flag_file = baseinfo["国旗画像"]
-    print(flag_file.replace(' ','_'))
+    url = 'https://commons.wikimedia.org/w/api.php?action=query&titles=File:' + \
+        flag_file + '&prop=imageinfo&iiprop=url&format=json'
+    data = requests.get(url)
+    flag_url = re.search(r'"url":"(.+?)"', data.text).group(1)
+    print(flag_url)
---
+++ takuo/chapter04/knock34.py
@@ -0,0 +1,23 @@
+from knock30 import get_mecabed_list
+mecabed = get_mecabed_list("chapter04/neko.txt.mecab")
+
+for line_idx in range(len(mecabed)):
+    dict_idx = 0
+    tmpstr = ""
+    count = 0
+    while (dict_idx < len(mecabed[line_idx])):
+        dic = mecabed[line_idx][dict_idx]
+        if (dic["pos"] == "名詞"):
+            tmpstr += dic["surface"]
+            count += 1
+
+        elif (tmpstr != ""):
+            if (count > 1):
+                print(tmpstr, count)
+            tmpstr = ""
+            count = 0
+        else:
+            tmpstr = ""
+            count = 0
+
+        dict_idx += 1
