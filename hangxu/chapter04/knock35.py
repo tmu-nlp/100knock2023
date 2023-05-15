9bc55719e7824ad46dd05d11927453c77e9a25c5
zhou-hangxu <131327688+zhou-hangxu@users.noreply.github.com>
2023-05-15 15:42:40 +0900

    Update knock35.py

--- hangxu/chapter04/knock35.py
+++ hangxu/chapter04/knock35.py
@@ -4,7 +4,7 @@ from knock30 import result
 words = []  
 for word in result:
     for a in word:
-        words.append(a['base'])
+        words.append(a['surface'])
 
 
 from collections import Counter
