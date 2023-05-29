def3e389784341acb493636def87179f182c12af
nevucide <nevul2090@gmail.com>
2023-05-29 13:52:44 +0900

    song

---
+++ song/chapter06/knock50.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+# データの読込
+df = pd.read_csv('./song/chapter06/newsCorpora_re.csv', header=None, sep='\t', names=['ID', 'TITLE', 'URL', 'PUBLISHER', 'CATEGORY', 'STORY', 'HOSTNAME', 'TIMESTAMP'])
+
+# データの抽出
+df = df.loc[df['PUBLISHER'].isin(['Reuters', 'Huffington Post', 'Businessweek', 'Contactmusic.com', 'Daily Mail']), ['TITLE', 'CATEGORY']]
+
+# データの分割
+train, valid_test = train_test_split(df, test_size=0.2, shuffle=True, random_state=123, stratify=df['CATEGORY'])
+valid, test = train_test_split(valid_test, test_size=0.5, shuffle=True, random_state=123, stratify=valid_test['CATEGORY'])
+
+# データの保存
+train.to_csv('./song/chapter06/train.txt', sep='\t', index=False)
+valid.to_csv('./song/chapter06/valid.txt', sep='\t', index=False)
+test.to_csv('./song/chapter06/test.txt', sep='\t', index=False)
+
+# 事例数の確認
+print('【学習データ】')
+print(train['CATEGORY'].value_counts())
+print('【検証データ】')
+print(valid['CATEGORY'].value_counts())
+print('【評価データ】')
+print(test['CATEGORY'].value_counts())
