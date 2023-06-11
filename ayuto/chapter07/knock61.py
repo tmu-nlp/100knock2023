16b738b156eb98f98ff4fe1691c3c4fe38996110
Ayuto1215 <Ayuto1215@outlook.com>
2023-06-11 18:23:47 +0900

    a

---
+++ ayuto/chapter07/knock61.py
@@ -0,0 +1,3 @@
+from gensim.models import KeyedVectors
+model = KeyedVectors.load_word2vec_format(r"C:\Users\Ayuto\Downloads\GoogleNews-vectors-negative300.bin.gz",binary=True)
+print(model.similarity('United_States', 'U.S.'))
